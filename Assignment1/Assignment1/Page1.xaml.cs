7b07611c2ea774bb4a62096bb50f31baa0e4f2a0
Jarred <bettersoundingonhugs@gmail.com>
2019-04-04 14:29:38 +1300

    second commit

---
+++ Assignment1/Assignment1/Page1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Assignment1
+{
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Handle_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page2());
+        }
+
+        private void Handle_Clicked2(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page3());
+        }
+    }
+}
